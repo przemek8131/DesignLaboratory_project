f53f0f4c64c2722542620d125aec04b25c7bbb8e
przemek8131 <56112701+przemek8131@users.noreply.github.com>
2021-12-21 17:33:41 +0100

    Almost final code

--- code/main.c
+++ code/main.c
@@ -6,13 +6,22 @@
  */ 
 
 #define F_CPU 16000000UL
-//
+
 #define BAUD 9600UL
 #define UBRR_VAL ((F_CPU+BAUD*8)/(BAUD*16)-1)
+#define Button_1 12
+#define Button_2 24
+#define Button_3 94
+#define Button_4 8
+#define Button_5 28
+#define Button_6 90
+#define Button_7 66
+#define Button_8 82
+#define Button_9 74
+#define Power_OFF 69
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
-//
 #include <stdlib.h>
 #include <util/setbaud.h>
 #include <util/delay.h>
@@ -26,7 +35,7 @@ volatile uint8_t komenda_negacja = 0;	//komenda przes³ana przez pilot
 volatile uint8_t error = 0; // flaga bledu odbioru
 volatile uint8_t przytrzymanie = 0;
 char rx_buf[]={0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20,0x20};
-//
+
 
 void uart_putc(unsigned char c){
 	while(!(UCSR0A & (1<<UDRE0)))
@@ -53,6 +62,30 @@ void timer1_init(void){
 	TIMSK1 |= (1<<ICIE1);	//W³¹czenie przerwania w trybie input capture
 }
 
+void react(void){
+	switch(komenda){
+		case Button_1:
+			PORTD ^= (1 << 2);
+		break;
+		case Button_2:
+			PORTD ^= (1 << 3);
+		break;
+		case Button_3:
+			PORTD ^= (1 << 4);
+		break;
+		case Button_4:
+			PORTD ^= (1 << 5);
+		break;
+		case Power_OFF:
+			PORTD &= ~((1 << 2) | (1 << 3) | (1 << 4) | (1 << 5));
+		break;
+	}
+}
+
+void relays_init(void){
+	DDRD = (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5);
+}
+
 ISR(TIMER1_CAPT_vect){
 	if(flaga==0){
 		pomiary[licznik] = ICR1/2;	//zapisanie przechwyconego czasu, przez 2 aby by³a dok³adnoœæ co do 1us
@@ -77,6 +110,7 @@ int main(void)
 {	
 	timer1_init();
 	uart_init();
+	relays_init();
 
 	sei();	//W³¹czenie globalnych przerwañ
 	
@@ -87,7 +121,9 @@ int main(void)
 		przytrzymanie = 0;
 		}
 		if(flaga == 1){
-			_delay_ms(200);
+			_delay_ms(200);		/* w przypadku odebrania jedynie fragmentu sygna³u przy ponownym wciœniêciu przycisku odebrana zosta³aby nie pe³na wiadomoœæ, 
+								   a kolejne przerwania inkrementowa³yby niepoprawnie licznik, opóŸnienie powoduje pominiêcie wiêkszej liczby przerwañ ni¿ 34, 
+								   licznik zostanie poprawnie wyzerowany i nastêpna komenda bêdzie poprawnie odebrana*/
 			if (4500 <= pomiary[1] && pomiary[1] <= 5500){    //sprawdzenie czy rozpoznana zosta³a poprawna sekwencja, pierwszy czas to odstêp pomiêdzy kolejnymi komendami
 				for(int i = 18; i < 26; i++){
 					if(900 <= pomiary[i] && pomiary[i] <=1500){
@@ -116,6 +152,7 @@ int main(void)
 					}
 				}
 				if((!error) && (komenda == komenda_negacja)){
+					react();
 					sprintf(rx_buf,"%d\n\r",komenda);
 					uart_puts(rx_buf);
 				}
@@ -127,9 +164,8 @@ int main(void)
 			}
 			flaga = 0;
 			sprintf(rx_buf,"-------\n\r");
-			uart_puts(rx_buf);
-			
-		//if(flaga==1){		//wyswietlanie wszystkich pomiarow
+			uart_puts(rx_buf);	
+			//if(flaga==1){		//wyswietlanie wszystkich pomiarow
 			/*for(int i = 0;i < 34;i++){
 			sprintf(rx_buf,"%d\n\r",pomiary[i]);
 			uart_puts(rx_buf);
@@ -138,7 +174,6 @@ int main(void)
 			sprintf(rx_buf,"------\n\r");
 			uart_puts(rx_buf);
 			//flaga = 0;*/
-		
 		}
 	}
 }
